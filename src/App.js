e9a3e339044d1c0705382cc6d369f50d280b9ede
West <agus2kcoin@outlook.com>
2022-11-18 16:28:11 -0300

    Implemented List and buttons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
-import './App.css';
+import React, { useState, useEffect } from "react";
+import PokemonList from "./PokemonList";
+import axios from "axios";
+import Pagination from "./Pagination";
 
 function App() {
+  const [pokemon, setPokemon] = useState([]);
+  const [currentPageUrl, setCurrentPageURL] = useState("https://pokeapi.co/api/v2/pokemon");
+  const [nextPageUrl, setNextPageUrl] = useState();
+  const [prevPageUrl, setPrevPageUrl] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    let cancel;
+    axios.get(currentPageUrl, {
+      cancelToken: new axios.CancelToken(c => cancel = c)
+    }).then(response => {
+      setLoading(false);
+      setNextPageUrl(response.data.next);
+      setPrevPageUrl(response.data.previous);
+      setPokemon(response.data.results.map(p => p.name));
+    });
+
+    return () => cancel();
+  }, [currentPageUrl]);
+
+  function gotoNextPage() {
+    setCurrentPageURL(nextPageUrl);
+  }
+
+  function gotoPrevPage() {
+    setCurrentPageURL(prevPageUrl);
+  }
+
+  if (loading) return "Loading...";
+
   return (
-    null
+    <>
+      <PokemonList pokemon={pokemon} />
+      <Pagination gotoNextPage={nextPageUrl ? gotoNextPage : null} gotoPrevPage={prevPageUrl ? gotoPrevPage : null} />
+    </>
   );
 }
 
